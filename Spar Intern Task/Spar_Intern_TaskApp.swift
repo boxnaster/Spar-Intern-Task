6ef1c991f92b7373753b672603020a04114f0169
boxnaster <nastya3362@yandex.ru>
2024-08-06 13:42:40 +0200

    Initial Commit

---
+++ Spar Intern Task/Spar_Intern_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spar_Intern_TaskApp.swift
+//  Spar Intern Task
+//
+//  Created by Анастасия Тимофеева on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Spar_Intern_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
